b528c805dbb5bb965ee0c470f9a1f0d51ea6883e
droidroy-ai <roy.abhik.dev@gmail.com>
2022-06-12 17:36:55 +0530

    login form initial style

--- base/forms.py
+++ base/forms.py
@@ -1,10 +1,36 @@
 from django import forms
 
 class RegisterForm(forms.Form):
-    username = forms.CharField()
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput)
+    username = forms.CharField(
+            widget=forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Your username"
+                }
+            )
+    )
+    email = forms.EmailField(
+            widget=forms.EmailInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Your email"
+                }
+            )
+    )
+    password = forms.CharField(
+            widget=forms.PasswordInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Enter password..."
+                }
+            ))
+    password2 = forms.CharField(label="Confirm Password", 
+            widget=forms.PasswordInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Confirm password..."
+                }
+    ))
 
     def clean(self):
         data = self.cleaned_data
